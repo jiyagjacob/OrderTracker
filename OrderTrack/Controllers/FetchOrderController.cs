a250654fb8530da963d807a8df4279325c41be71
Jiya <jiyagjacob@gmail.com>
2020-07-30 20:48:31 +1000

    Initial Commit

---
+++ OrderTrack/Controllers/FetchOrderController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrderTrack.Controllers
+{
+	public class FetchOrderController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View("~/Views/FetchOrder/FetchOrder.cshtml");
+		}
+		public IActionResult LoadMain()
+		{
+			return View("~/Views/FetchOrder/FetchOrder.cshtml");
+		}
+	}
+}
